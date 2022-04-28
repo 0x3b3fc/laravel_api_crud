c1e6b5bc0bce04b689702511b1baada3e0e96fdd
0x3b3fc <dev.sief.hesham@gmail.com>
2022-04-28 17:56:03 +0200

    laravel api crud

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <title>Api Crud System</title>
+</head>
+<body>
+@include('nav')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card text-center mt-5">
+                <div class="card-header">
+                    <h4>Api Crud System Operation</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="{{ asset('assets/js/jquery.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+</body>
+</html>
